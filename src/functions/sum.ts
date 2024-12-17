a917c4fe06a81341870c8cab5c84fe29fe461cac
rnt1910 <rnt1910@mail.ru>
2024-12-17 22:10:22 +0300

    sum [solved]

--- src/functions/sum.ts
+++ src/functions/sum.ts
@@ -5,8 +5,9 @@
  *  - задай тип аргументам (num1 и num2)
  *  - задай возвращаемый тип функции (подсказка: `number`)
  */
-
-function sum(num1, num2) {}
+function sum(num1: number, num2: number) {
+  return num1 + num2;
+}
 
 console.log(sum(5, 5)); // 10
 
